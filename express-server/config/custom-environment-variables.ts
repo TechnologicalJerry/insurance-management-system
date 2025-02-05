35edf6e977907abf2d4e881d426d9fed13905892
technologicaljerry <cserajatverma@gmail.com>
2025-02-05 08:13:59 +0530

    Update custom-environment-variables.ts

--- express-server/config/custom-environment-variables.ts
+++ express-server/config/custom-environment-variables.ts
@@ -1,6 +1,10 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export default {
-  accessTokenPrivateKey: "ACCESS_TOKEN_PRIVATE_KEY",
-  accessTokenPublicKey: "ACCESS_TOKEN_PUBLIC_KEY",
-  refreshTokenPrivateKey: "REFRESH_PRIVATE_KEY",
-  refreshTokenPublicKey: "REFRESH_PUBLIC_KEY",
+  accessTokenPrivateKey: process.env.ACCESS_TOKEN_PRIVATE_KEY || "",
+  accessTokenPublicKey: process.env.ACCESS_TOKEN_PUBLIC_KEY || "",
+  refreshTokenPrivateKey: process.env.REFRESH_TOKEN_PRIVATE_KEY || "",
+  refreshTokenPublicKey: process.env.REFRESH_TOKEN_PUBLIC_KEY || "",
 };
